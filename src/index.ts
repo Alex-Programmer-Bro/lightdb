53da8bbbd66f8adf0dd4b54db10225016ba2a985
Alex-Programer <alex.lichangnan@gmail.com>
2023-09-04 14:55:34 +0800

    feat: simply support

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,67 @@
-export const one = 1
-export const two = 2
+import { mkdir, readFile, stat, writeFile } from 'node:fs/promises'
+import { dirname } from 'node:path'
+
+export class LightDB<T> {
+  private data: T[] = []
+
+  private cacheFilePath!: string
+
+  public async init(filepath: string) {
+    async function createDirectoryAndFile(filePath: string, data: T[]) {
+      const directoryPath = dirname(filePath)
+
+      try {
+        await mkdir(directoryPath, { recursive: true })
+        await writeFile(filePath, JSON.stringify(data))
+      }
+      catch (error) {
+        throw new Error((error as Error).message)
+      }
+    }
+
+    this.cacheFilePath = filepath
+
+    try {
+      const { isFile } = await stat(this.cacheFilePath)
+
+      if (isFile()) {
+        try {
+          const content = JSON.parse(await readFile(this.cacheFilePath, 'utf-8'))
+          if (Array.isArray(content))
+            this.data = content
+          else
+            throw new Error('must be array data')
+        }
+        catch (error) {
+          throw new Error((error as Error).message)
+        }
+      }
+      else {
+        throw new Error(`${this.cacheFilePath} is not a file type`)
+      }
+    }
+    catch (error) {
+      await createDirectoryAndFile(this.cacheFilePath, [])
+    }
+  }
+
+  public read() {
+    if (!this.cacheFilePath)
+      throw new Error('Must init first please')
+
+    return this.data
+  }
+
+  public async write(data: T[]) {
+    if (!this.cacheFilePath)
+      throw new Error('Must init first please')
+
+    try {
+      await writeFile(this.cacheFilePath, JSON.stringify(data))
+      this.data = data
+    }
+    catch (error) {
+      throw new Error((error as Error).message)
+    }
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,32 @@
-import { describe, expect, it } from 'vitest'
+import { beforeAll, expect, test } from 'vitest'
+import { LightDB } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
+const lightDB = new LightDB()
+const tempFoldPath = './.cache/code-snippets.json'
+
+beforeAll(async () => {
+  await lightDB.init(tempFoldPath)
+})
+
+test('get data', () => {
+  const snippets = lightDB.read()
+  expect(Array.isArray(snippets)).toBeTruthy()
+})
+
+test('set Array data', async () => {
+  await lightDB.write([])
+  const snippets = lightDB.read()
+  expect(snippets).toEqual([])
+
+  const data = [
+    {
+      id: 1,
+      name: 'hello',
+      age: 24,
+    },
+  ]
+  await lightDB.write(data)
+  expect(lightDB.read()).toEqual(data)
+
+  await lightDB.write([])
 })
